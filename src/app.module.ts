4a706ac2ad450548a87d64a7746783658757545d
Nithima44 <nitima7859@gmail.com>
2026-03-02 18:15:03 +0700

    01

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MissionController } from './mission/mission.controller';
+import { MissionService } from './mission/mission.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [MissionController],
+  providers: [MissionService],
 })
 export class AppModule {}
+
+
